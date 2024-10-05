bc83a89db4364ff791ad79d2cad5e4ef7cc6efcc
Anurudha Singh <singhanurudh570@gmail.com>
2024-10-05 20:40:14 +0530

    Token creation and validation are under progress

--- src/main/java/com/anurudh/SpringSec/configuration/SecurityConfiguration.java
+++ src/main/java/com/anurudh/SpringSec/configuration/SecurityConfiguration.java
@@ -1,33 +1,74 @@
 package com.anurudh.SpringSec.configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity. csrf(customizer -> customizer.disable());
-        httpSecurity.authorizeHttpRequests(request-> request.anyRequest().authenticated());
-        // httpSecurity.formLogin(Customizer.withDefaults()); //for web because it returns a default login form
-        httpSecurity.httpBasic(Customizer.withDefaults()); // it will prompt a popup on the web for authenticating the user
-        return httpSecurity.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(customizer -> customizer.disable()).
+                authorizeHttpRequests(request -> request
+                        .requestMatchers("login", "register").permitAll()
+                        .anyRequest().authenticated()).
+                httpBasic(Customizer.withDefaults()).
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+
+
+    }
+
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user1 = User
+//                .withDefaultPasswordEncoder()
+//                .username("kiran")
+//                .password("k@123")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User
+//                .withDefaultPasswordEncoder()
+//                .username("harsh")
+//                .password("h@123")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+
+
+        return provider;
     }
 
     @Bean
-    UserDetailsService userDetailsService(){
-        UserDetails user1=User.withDefaultPasswordEncoder().username("anurudh_singh").password("test@123").roles("ADMIN").build();
-        UserDetails user2=User.withDefaultPasswordEncoder().username("ankit").password("test@123").roles("USER").build();
-        
-        return new InMemoryUserDetailsManager(user1);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+
     }
+
+
 }
---
+++ src/main/java/com/anurudh/SpringSec/services/UserService.java
@@ -0,0 +1,41 @@
+package com.anurudh.SpringSec.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.anurudh.SpringSec.model.Users;
+import com.anurudh.SpringSec.repo.UserRepo;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    AuthenticationManager authManager;
+
+    @Autowired
+    private UserRepo repo;
+
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        repo.save(user);
+        return user;
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+   if (authentication.isAuthenticated()) {
+         return jwtService.generateToken()  ;
+        } else {
+            return "fail";
+        }
+    }
+}
